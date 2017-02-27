70cbe39aa94014e1dd0727edf035c6348bdee8ad
Pabs <pablo.monestel@gmail.com>
2017-02-27 17:39:04 -0600

    Commit del back-end

---
+++ back-end/api/config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'secret':'',
+  'database' : 'mongodb://root:root@ds145868.mlab.com:45868/user_test'
+}
+//Produccion
+  //'database' : 'mongodb://root:root@ds145868.mlab.com:45868/user_test'
+//Desarrollo
+  //'database' : 'mongodb://localhost/users-db'
